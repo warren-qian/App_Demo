befbcac68d5b506fdfed824ccef7241a3f7a933b
Warren Qian <warrenq@Warrens-MacBook-Pro.local>
2020-03-17 11:10:12 +0800

    d

--- demo/cypress/integration/Visit_Web.js
+++
@@ -1,5 +0,0 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    cy.visit('https://manage.proxy.com/settings/orgs/')
-  })
-})
--- task_proj/vision.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-url = 'https://projects.invisionapp.com/share/28TGKUT9Q5C#/screens/380446897'
-
-brower = webdriver.Chrome()
